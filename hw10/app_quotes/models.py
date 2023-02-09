e6d04b219b2c1e6b8595116cad0e218851cfc723
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-10 00:16:53 +0200

    Update models.py

--- hw10/app_quotes/models.py
+++ hw10/app_quotes/models.py
@@ -7,11 +7,30 @@ class Author(models.Model):
     born_location = CharField(max_length=100, null=False)
     bio = TextField(null=False)
 
+    def __str__(self):
+        return f'{self.fullname}'
+
 class Quote(models.Model):
     author = ForeignKey(Author, on_delete=CASCADE)
     text = TextField(unique=True, null=False)
     tags = ManyToManyField(Tag)
 
+    def __str__(self):
+        return f'{self.author}: {self.text}, {self.tags}'
+
+    def show_author(self):
+        return self.author
+
+    def show_text(self):
+        return self.text
+
+    def show_tags(self):
+        tag = self.tags.all()
+        return tag
+
 
 class Tag(models.Model):
     name = CharField(max_length=50, null=False, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
